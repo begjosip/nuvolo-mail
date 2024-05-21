a60580d173ad83412c83c7dc86f24db1768fdcdc
begjosip <josipbegic57@gmail.com>
2024-05-21 19:24:37 +0200

    Implement email sending via smtp server

--- email_service/email_service.py
+++ email_service/email_service.py
@@ -1,13 +1,18 @@
 import logging
 import os
 import smtplib
+from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email import encoders
+from jinja2 import Template
 
 
 class EmailService:
+    VERIFICATION_TEMPLATE_PATH = 'templates/verification.html'
+    NOTIFICATION_TEMPLATE_PATH = 'templates/notification.html'
+    PASSWORD_RESET_TEMPLATE_PATH = 'templates/password_reset_request.html'
+    LOGO_IMAGE_PATH = 'templates/images/nuvolo.png'
+
     def __init__(self):
         self._smtp_server = os.getenv("SMTP_SERVER")
         self._smtp_port = os.getenv("SMTP_PORT")
@@ -15,17 +20,81 @@ class EmailService:
         self._smtp_password = os.getenv("SMTP_PASSWORD")
         self.logger = logging.getLogger(__name__)
 
-    @staticmethod
-    def send_verification_email(verification):
-        print("Sending email to:")
-    #     TODO:
+    def send_verification_email(self, verification):
+        self.logger.info("Fetching template for verification")
+        with open(self.VERIFICATION_TEMPLATE_PATH, 'r') as file:
+            template = Template(file.read())
+        html_body = template.render({
+            'name': verification.firstName + ' ' + verification.lastName,
+            'verification_link': os.getenv("VERIFICATION_URI") + verification.token
+        })
+        msg = MIMEMultipart('related')
+        msg['From'] = self._smtp_mail
+        msg['To'] = verification.email
+        msg['Subject'] = 'Verify your account'
+        msg.attach(MIMEText(html_body, 'html'))
+        with open(self.LOGO_IMAGE_PATH, 'rb') as img_file:
+            img = MIMEImage(img_file.read())
+            img.add_header('Content-ID', '<nuvolo>')
+            img.add_header('Content-Disposition', 'inline',
+                           filename=os.path.basename(self.LOGO_IMAGE_PATH))
+            msg.attach(img)
+            self.logger.debug("Logging to SMTP server")
+            server = smtplib.SMTP_SSL(self._smtp_server, int(self._smtp_port))
+            server.login(self._smtp_mail, self._smtp_password)
+            server.sendmail(self._smtp_mail, verification.email, msg.as_string())
+            self.logger.info("Successfully sent email to {}", verification.email)
+            server.quit()
 
-    @staticmethod
-    def send_pass_reset_email(pass_reset):
-        print("Sending email to:")
-    #     TODO:
+    def send_pass_reset_email(self, pass_reset):
+        self.logger.info("Fetching template for password reset email")
+        with open(self.PASSWORD_RESET_TEMPLATE_PATH, 'r') as file:
+            template = Template(file.read())
+        html_body = template.render({
+            'name': pass_reset.firstName + ' ' + pass_reset.lastName,
+            'pass_reset_link': os.getenv("PASS_RESET_URI") + pass_reset.token
+        })
+        msg = MIMEMultipart('related')
+        msg['From'] = self._smtp_mail
+        msg['To'] = pass_reset.email
+        msg['Subject'] = 'Forgot your password. No problem!'
+        msg.attach(MIMEText(html_body, 'html'))
+        with open(self.LOGO_IMAGE_PATH, 'rb') as img_file:
+            img = MIMEImage(img_file.read())
+            img.add_header('Content-ID', '<nuvolo>')
+            img.add_header('Content-Disposition', 'inline',
+                           filename=os.path.basename(self.LOGO_IMAGE_PATH))
+            msg.attach(img)
+            self.logger.debug("Logging to SMTP server")
+            server = smtplib.SMTP_SSL(self._smtp_server, int(self._smtp_port))
+            server.login(self._smtp_mail, self._smtp_password)
+            server.sendmail(self._smtp_mail, pass_reset.email, msg.as_string())
+            self.logger.info("Successfully sent email to {}", pass_reset.email)
+            server.quit()
 
-    @staticmethod
-    def send_notification_email(notification):
-        print("Sending email to:")
-    #     TODO:
+    def send_notification_email(self, notification):
+        self.logger.info("Fetching template for verification")
+        with open(self.VERIFICATION_TEMPLATE_PATH, 'r') as file:
+            template = Template(file.read())
+        html_body = template.render({
+            'name': notification.firstName + ' ' + notification.lastName,
+            'main_page_url': os.getenv("MAIN_PAGE_URI"),
+            'message': notification.message
+        })
+        msg = MIMEMultipart('related')
+        msg['From'] = self._smtp_mail
+        msg['To'] = notification.email
+        msg['Subject'] = 'Verify your account'
+        msg.attach(MIMEText(html_body, 'html'))
+        with open(self.LOGO_IMAGE_PATH, 'rb') as img_file:
+            img = MIMEImage(img_file.read())
+            img.add_header('Content-ID', '<nuvolo>')
+            img.add_header('Content-Disposition', 'inline',
+                           filename=os.path.basename(self.LOGO_IMAGE_PATH))
+            msg.attach(img)
+            self.logger.debug("Logging to SMTP server")
+            server = smtplib.SMTP_SSL(self._smtp_server, int(self._smtp_port))
+            server.login(self._smtp_mail, self._smtp_password)
+            server.sendmail(self._smtp_mail, notification.email, msg.as_string())
+            self.logger.info("Successfully sent email to {}", notification.email)
+            server.quit()
--- rabbitmq/rabbitmq_listener.py
+++ rabbitmq/rabbitmq_listener.py
@@ -14,72 +14,7 @@ class RabbitMQListener:
         self._connection = pika.BlockingConnection(self._parameters)
         self._channel = self._connection.channel()
         self.logger = logging.getLogger(__name__)
-
-    def __verification_callback(self, ch, method, properties, body):
-        verification = json.loads(body)
-        try:
-            EmailService.send_verification_email(verification)
-            self.logger.info("Successfully sent verification email.")
-            verification['success'] = 'true'
-            self._channel.basic_publish(
-                exchange='',
-                routing_key=properties.reply_to,
-                properties=pika.BasicProperties(correlation_id=properties.correlation_id),
-                body=json.dumps(verification).encode('utf-8'))
-            self.logger.info("Acknowledging verification email.")
-            ch.basic_ack(delivery_tag=method.delivery_tag)
-        except Exception as ex:
-            self._channel.basic_publish(
-                exchange='',
-                routing_key=properties.reply_to,
-                properties=pika.BasicProperties(correlation_id=properties.correlation_id),
-                body=json.dumps(verification).encode('utf-8'))
-            self.logger.info("Acknowledging failed message.")
-            ch.basic_ack(delivery_tag=method.delivery_tag)
-
-    def __password_reset_callback(self, ch, method, properties, body):
-        pass_reset_request = json.loads(body)
-        try:
-            EmailService.send_pass_reset_email(pass_reset_request)
-            self.logger.info("Successfully sent password reset request email.")
-            pass_reset_request['success'] = 'true'
-            self._channel.basic_publish(
-                exchange='',
-                routing_key=properties.reply_to,
-                properties=pika.BasicProperties(correlation_id=properties.correlation_id),
-                body=json.dumps(pass_reset_request).encode('utf-8'))
-            self.logger.info("Acknowledging password reset email.")
-            ch.basic_ack(delivery_tag=method.delivery_tag)
-        except Exception as ex:
-            self._channel.basic_publish(
-                exchange='',
-                routing_key=properties.reply_to,
-                properties=pika.BasicProperties(correlation_id=properties.correlation_id),
-                body=json.dumps(pass_reset_request).encode('utf-8'))
-            self.logger.info("Acknowledging failed message.")
-            ch.basic_ack(delivery_tag=method.delivery_tag)
-
-    def __notification_callback(self, ch, method, properties, body):
-        notification = json.loads(body)
-        try:
-            EmailService.send_notification_email(notification)
-            self.logger.info("Successfully sent notification email.")
-            notification['success'] = 'true'
-            self._channel.basic_publish(
-                exchange='',
-                routing_key=properties.reply_to,
-                properties=pika.BasicProperties(correlation_id=properties.correlation_id),
-                body=json.dumps(notification).encode('utf-8'))
-            self.logger.info("Acknowledging notification email.")
-            ch.basic_ack(delivery_tag=method.delivery_tag)
-        except Exception as ex:
-            self._channel.basic_publish(
-                exchange='',
-                routing_key=properties.reply_to,
-                properties=pika.BasicProperties(correlation_id=properties.correlation_id),
-                body=json.dumps(notification).encode('utf-8'))
-            self.logger.info("Acknowledging failed message.")
-            ch.basic_ack(delivery_tag=method.delivery_tag)
+        self._email_service = EmailService()
 
     def start_listening(self):
         self._channel.basic_qos(prefetch_count=1)
@@ -91,3 +26,50 @@ class RabbitMQListener:
                                     on_message_callback=self.__notification_callback)
         self.logger.info("Starting RabbitMQ listener consuming.")
         self._channel.start_consuming()
+
+    def __verification_callback(self, ch, method, properties, body):
+        verification = json.loads(body)
+        try:
+            self.logger.info("Calling email service for verification email sending.")
+            self._email_service.send_verification_email(verification)
+            self.logger.info("Successfully sent verification email.")
+            verification['success'] = 'true'
+            self.__send_acknowledged_message(ch, method, properties, verification)
+        except Exception as ex:
+            verification['success'] = 'false'
+            self.logger.error("Error occurred while sending verification email. {}", ex)
+            self.__send_acknowledged_message(ch, method, properties, verification)
+
+    def __password_reset_callback(self, ch, method, properties, body):
+        pass_reset_request = json.loads(body)
+        try:
+            self.logger.info("Calling email service for verification email sending.")
+            EmailService.send_pass_reset_email(pass_reset_request)
+            self.logger.info("Successfully sent password reset request email.")
+            pass_reset_request['success'] = 'true'
+            self.__send_acknowledged_message(ch, method, properties, pass_reset_request)
+        except Exception as ex:
+            pass_reset_request['success'] = 'false'
+            self.logger.error("Error occurred while sending password reset request. {}", ex)
+            self.__send_acknowledged_message(ch, method, properties, pass_reset_request)
+
+    def __notification_callback(self, ch, method, properties, body):
+        notification = json.loads(body)
+        try:
+            EmailService.send_notification_email(notification)
+            self.logger.info("Successfully sent notification email.")
+            notification['success'] = 'true'
+            self.__send_acknowledged_message(ch, method, properties, notification)
+        except Exception as ex:
+            notification['success'] = 'false'
+            self.logger.error("Error occurred while sending notification email. {}", ex)
+            self.__send_acknowledged_message(ch, method, properties, notification)
+
+    def __send_acknowledged_message(self, ch, method, properties, message):
+        self._channel.basic_publish(
+            exchange='',
+            routing_key=properties.reply_to,
+            properties=pika.BasicProperties(correlation_id=properties.correlation_id),
+            body=json.dumps(message).encode('utf-8'))
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+        self.logger.info("Successfully acknowledged message")
